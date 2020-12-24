454c32aeadc59261ee3c6937d53524bc57f07308
LuukEbenau <l.ebenau@hotmail.com>
2020-12-24 12:46:28 +0100

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {
   Link
 } from "react-router-dom";
 
+import './App.scss'
+
 import About from './pages/About'
 import Home from './pages/Home'
 import Leden from './pages/Leden'
@@ -13,21 +15,27 @@ import Leden from './pages/Leden'
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/leden">Onze leden</Link>
-            </li>
-          </ul>
-        </nav>
+      <div>  
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about"  className="nav-link">About</Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link to="/leden" className="nav-link">Onze leden</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
