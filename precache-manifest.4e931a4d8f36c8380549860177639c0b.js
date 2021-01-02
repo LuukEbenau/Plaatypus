18ea2828fe0b9544bf907821a81a8d7017fc894c
LuukEbenau <l.ebenau@hotmail.com>
2021-01-02 16:48:08 +0100

    Updates

---
+++ precache-manifest.4e931a4d8f36c8380549860177639c0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b99dc347ca916be042bc6ca6d9c1f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9520c29d52971c1fdc0a",
+    "url": "/static/css/main.b383c719.chunk.css"
+  },
+  {
+    "revision": "24d329f9d1616f6d41ef",
+    "url": "/static/js/2.7991b72f.chunk.js"
+  },
+  {
+    "revision": "f9689ab3bdd81059c30c22857445580d",
+    "url": "/static/js/2.7991b72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9520c29d52971c1fdc0a",
+    "url": "/static/js/main.5911f998.chunk.js"
+  },
+  {
+    "revision": "509df4453cf433dabd7c",
+    "url": "/static/js/runtime-main.4998cf42.js"
+  },
+  {
+    "revision": "a06d3f1a949ca6252e5ec5c8de4d4d6d",
+    "url": "/static/media/2.a06d3f1a.png"
+  },
+  {
+    "revision": "2235438fdfd582af08469029ac7ba5b2",
+    "url": "/static/media/fabian.2235438f.png"
+  },
+  {
+    "revision": "77431a9bf3f4cd60f48b51a65ffae9be",
+    "url": "/static/media/geile frederik.77431a9b.jpg"
+  },
+  {
+    "revision": "b7fbf71c3e52fcb4511d1c0efa83b56e",
+    "url": "/static/media/logo.b7fbf71c.png"
+  },
+  {
+    "revision": "f7320c00da2f003adbf6a62a08e97c9f",
+    "url": "/static/media/mongooltjes.f7320c00.jpg"
+  }
+]);
