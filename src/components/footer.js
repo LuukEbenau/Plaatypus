c28b85a5cff9630e81cf628854dc25a88f0cb2c1
the1Rogue <fabiantschut@gmail.com>
2021-01-02 18:57:30 +0100

    Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../styles/footer.scss'
+import insta from "../images/insta.png"
 export default function Footer() {
     return (
         <div className="footer">
-            <p>copyright Plaatypus @2020</p>
+            <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">copyright Plaatypus @2020</a></p>
             <div className="social">
-                <i className="fa fa-facebook"></i>
-                <i className="fa fa-instagram"></i>
+                <a href="https://www.instagram.com/demosjc_i.o._6"><img className="fa fa-instagram" src={insta} alt="instagram"/></a>
             </div>
         </div>
     )
